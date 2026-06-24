7bef67b66614abbe2445e325c39257aac5e07c5a
kayamavy <150877815+maryoa61@users.noreply.github.com>
2026-06-24 10:21:23 +0330

    Create DeployCounter contract for Counter deployment

---
+++ script/Deploy.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {Counter} from "../src/Counter.sol";
+
+contract DeployCounter is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        Counter counter = new Counter();
+
+        vm.stopBroadcast();
+    }
+}
